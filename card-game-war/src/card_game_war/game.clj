0d9e5208f69264680cebd4890a58002a9b65cf37
Drew Kitch <drew.kitch@gmail.com>
2016-10-29 18:09:23 -0600

    card game war solution

--- card-game-war/src/card_game_war/game.clj
+++ card-game-war/src/card_game_war/game.clj
@@ -8,6 +8,29 @@
         rank ranks]
     [suit rank]))
 
-(defn play-round [player1-card player2-card])
+(defn rank-position [ranking-list rank-val]
+  (.indexOf ranking-list rank-val))
 
-(defn play-game [player1-cards player2-cards])
+(defn card-rank [card]
+  (rank-position ranks (last card)))
+
+(defn suit-rank [card]
+  (rank-position suits (first card)))
+
+(defn play-round [player1-card player2-card]
+  (let [player1-card-rank (card-rank player1-card)
+        player1-suit-rank (suit-rank player1-card)
+        player2-card-rank (card-rank player2-card)
+        player2-suit-rank (suit-rank player2-card)]
+    (if (= player1-card-rank player2-card-rank) (if (> player1-suit-rank player2-suit-rank) 0 1)
+        (if (> player1-card-rank player2-card-rank) 0 1))))
+
+(defn play-game [player1-cards player2-cards]
+  (loop [player1-deck player1-cards player2-deck player2-cards]
+    (cond (zero? (count player2-deck)) 0 ;; Player 1 wins
+          (zero? (count player1-deck)) 1 ;; Player 2 wins
+          :else (let [round-winner (play-round (first player1-deck) (first player2-deck))
+                      player1-winning-deck (reverse (cons (first player1-deck) (cons (first player2-deck) (reverse (rest player1-deck)))))
+                      player2-winning-deck (reverse (cons (first player2-deck) (cons (first player1-deck) (reverse (rest player2-deck)))))]
+            (if (= 0 round-winner) (recur player1-winning-deck (rest player2-deck))
+                                   (recur (rest player1-deck) player2-winning-deck))))))
