2b35cc5dc967a22c9b2db634f48963cfe92da35a
Drew Kitch <drew.kitch@gmail.com>
2016-11-13 18:00:41 -0700

    WIP

--- doublets/src/doublets/solver.clj
+++ doublets/src/doublets/solver.clj
@@ -5,7 +5,24 @@
 (def words (-> "words.edn"
                (io/resource)
                (slurp)
-               (read-string)))
+               (read-string)
+               (set)))
+
+(defn all-possible-char-shifts-from-word [word]
+  (loop [index 0 char-shifted-words []]
+    (if (>= index (count word)) char-shifted-words
+        (let [char-int (int (.charAt word counter))]
+        ;; charAt doesn't account for all potential locations of char!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+          (let [upchar-the-word-at-index (str )
+                downcar-the-word-at-index ()]
+            (recur (inc index) (conj char-shifted-words upchar-the-word-at-index downchar-the-word-at-index)))))))
+
+(defn all-recognized-words-from-set)
 
 (defn doublets [word1 word2]
-  "make me work")
+  (if (not= (count word1) (count word2)) []
+      (loop [pathed-word word1 destination-word word2 doublets-found []]
+        (if (= pathed-word destination-word) (conj doublets-found destination-word)
+            (let [next-word (word-with-single-char-change pathed-word doublets-found)]
+              (if (nil? next-word) []
+                  (recur next-word destination-word (conj doublets-found pathed-word))))))))
